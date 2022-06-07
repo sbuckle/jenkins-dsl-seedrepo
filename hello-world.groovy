e7dde2dd71e7ba2841a1dc99406f0ec5b00e240d
Simon Buckle <simon.buckle@gmail.com>
2022-06-07 13:13:47 +0100

    Added checkout step

--- hello-world.groovy
+++ hello-world.groovy
@@ -2,6 +2,16 @@ pipeline {
     agent any
 
     stages {
+    	stage('Checkout') {
+    		steps {
+    			checkout([
+    				$class: 'GitSCM',
+    				branches: [['refs/heads/master']],
+    				extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'Test']],
+    				userRemoteConfigs: [[url: 'https://github.com/sbuckle/jenkins-dsl-seedrepo.git']]
+    			])
+    		}
+    	}
         stage('Hello') {
             steps {
                 echo "Hello ${NAME}"
